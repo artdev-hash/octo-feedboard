56ac5c6f9edb2f6139d10538a7fa998a304cf59b
artdev-hash <kim.dev.pr@gmail.com>
2023-04-18 18:35:48 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env sh
+
+set -e
+
+npm run docs:build
+
+cd docs/.vitepress/dist
+
+# if you are deploying to a custom domain
+#echo 'www.lorenzokimarthur.com' > CNAME
+
+git init
+git add -A 
+git commit -m 'deploy'
+
+git push -f git@github.com:artdev-hash/octo-feedboard.git master:gh-pages
+
+cd -
+
