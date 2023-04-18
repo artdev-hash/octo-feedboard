beaa52af2b2225644688fb8105161c199b249a48
artdev-hash <kim.dev.pr@gmail.com>
2023-04-18 12:22:05 +0800

    update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "octo-feedboard",
   description: "A Vite Project Board",
+  base:'/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
